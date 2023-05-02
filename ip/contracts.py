5938063747272b1915265b76413d8f40f9d7b472
LinealNan <linealnan@gmail.com>
2023-05-02 11:45:53 +0500

    [NF] Убран метод поиска и сохраниния данный ip в контракте

--- ip/contracts.py
+++ ip/contracts.py
@@ -6,8 +6,3 @@ class AbstractIpInfoRepository(metaclass=ABCMeta):
     def find_by_ip(self, ip: str):
         """Получение данных IP адреса"""
         pass
-
-    @abstractmethod
-    def find_and_save_by_ip(self, ip: str):
-        """Получение данных IP адреса"""
-        pass
